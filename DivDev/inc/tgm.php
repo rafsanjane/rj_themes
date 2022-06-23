8a49837912790509f74dd2981051ee98ed31eacd
rafsanjane <rafsanjanihoby@gmail.com>
2022-06-23 10:17:12 +0600

    ACF framwork

--- DivDev/inc/tgm.php
+++ DivDev/inc/tgm.php
@@ -30,9 +30,9 @@
  * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
  *
  * Plugin:
- * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
+ * require_once dirname( __FILE__ ) . '/lib/class-tgm-plugin-activation.php';
  */
-require_once dirname(__FILE__) . '/class-tgm-plugin-activation.php';
+require_once get_theme_file_path() . '/class-tgm-plugin-activation.php';
 
 add_action('tgmpa_register', 'my_theme_register_required_plugins');
 
@@ -80,7 +80,7 @@ function my_theme_register_required_plugins()
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
 	$config = array(
-		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'id'           => 'divdev-required-plugins',                 // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
 		'parent_slug'  => 'themes.php',            // Parent menu slug.
